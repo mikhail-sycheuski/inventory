26a6baaefa0678b68d01e55d16f873242d19d75c
mikhail_sycheuski <mikhail_sycheuski@epam.com>
2021-07-02 19:38:30 +0200

    Initialized repo with project skeleton

---
+++ src/test/kotlin/com/mikhailsycheuski/test/warehouse/WarehouseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mikhailsycheuski.test.warehouse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
